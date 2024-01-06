9c69132a14ef6b6afb087f0e9a5fb87f89f35ecd
safuraja7 <rajasaifi49@gmail.com>
2024-01-06 17:38:46 +0500

    initial commit

---
+++ lib/ui/home/home.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(11.0),
+          child: Image(
+            image: AssetImage('assets/png/menu.png'),
+          ),
+        ),
+        title: const Text("Location"),
+        centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Image(
+              height: height * .04,
+              image: const AssetImage('assets/png/setting.png'),
+            ),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Gap(10),
+              Center(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  height: height * .5,
+                  width: width * .9,
+                ),
+              ),
+              const Gap(10),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      height: height * .07,
+                      width: width * .3,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    const Gap(10),
+                    Container(
+                      height: height * .07,
+                      width: width * .3,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    const Gap(10),
+                    Container(
+                      height: height * .07,
+                      width: width * .3,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    const Gap(10),
+                    Container(
+                      height: height * .07,
+                      width: width * .3,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    const Gap(10),
+                    Container(
+                      height: height * .07,
+                      width: width * .3,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    const Gap(10),
+                    Container(
+                      height: height * .07,
+                      width: width * .3,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
